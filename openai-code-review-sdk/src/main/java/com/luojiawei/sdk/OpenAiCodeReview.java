189bf86dd2b4531521d1a2fd177c22ca09285ab7
luojiawei <2240774934@qq.com>
2025-07-05 17:49:48 +0800

    feat: init

---
+++ openai-code-review-sdk/src/main/java/com/luojiawei/sdk/OpenAiCodeReview.java
@@ -0,0 +1,7 @@
+package com.luojiawei.sdk;
+
+public class OpenAiCodeReview {
+    public static void main(String[] args) {
+        System.out.println("Hello OpenAI Code Review SDK!");
+    }
+}
---
+++ openai-code-review-test/src/main/java/com/luojiawei/Application.java
@@ -0,0 +1,5 @@
+package com.luojiawei;
+
+public class Application {
+
+}
