6a9358c617e4c14495ff22037b045bd6ee9d08ad
luojiawei <2240774934@qq.com>
2025-07-06 13:12:25 +0800

    feat: 代码评审

--- openai-code-review-test/src/main/java/com/luojiawei/Application.java
+++ openai-code-review-test/src/main/java/com/luojiawei/Application.java
@@ -1,4 +1,12 @@
 package com.luojiawei;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
 public class Application {
+    @Test
+    public void test(){
+        System.out.println("12341234567890");
+    }
 }
