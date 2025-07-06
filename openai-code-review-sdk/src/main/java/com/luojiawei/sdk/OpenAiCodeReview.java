a8a8d08f92ee3cfb2058f313d7ba59590038d2f4
luojiawei <2240774934@qq.com>
2025-07-06 13:43:22 +0800

    feat: 代码评审 v4.2

--- openai-code-review-sdk/src/main/java/com/luojiawei/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/com/luojiawei/sdk/OpenAiCodeReview.java
@@ -1,11 +1,13 @@
 package com.luojiawei.sdk;
 
 import com.alibaba.fastjson2.JSON;
+import com.luojiawei.sdk.domain.model.ChatCompletionRequest;
 import com.luojiawei.sdk.domain.model.ChatCompletionSyncResponse;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -45,18 +47,25 @@ public class OpenAiCodeReview {
         connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
         // 设置连接超时和读取超时
         connection.setDoOutput(true);
-        String jsonInpuString = "{"
-                + "\"model\":\"deepseek/deepseek-v3-0324\","
-                + "\"messages\": ["
-                + "    {"
-                + "        \"role\": \"user\","
-                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
-                + "    }"
-                + "]"
-                + "}";
+//        String jsonInpuString = "{"
+//                + "\"model\":\"deepseek/deepseek-v3-0324\","
+//                + "\"messages\": ["
+//                + "    {"
+//                + "        \"role\": \"user\","
+//                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+//                + "    }"
+//                + "]"
+//                + "}";
 
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel("deepseek/deepseek-v3-0324");
+
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){{
+            add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: "));
+            add(new ChatCompletionRequest.Prompt("user", code));
+        }});
         try(OutputStream os = connection.getOutputStream()) {
-            byte[] input = jsonInpuString.getBytes("utf-8");
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes("utf-8");
             os.write(input);
         }
 
